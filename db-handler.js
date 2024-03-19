317875abd991c6b2bec5a8b832312818b08ac4ce
edmond-chu <edmond.chu1999@gmail.com>
2024-03-19 18:44:59 -0400

    revert local db changes

--- db-handler.js
+++ db-handler.js
@@ -2,23 +2,14 @@ require('dotenv').config();
 const { Pool } = require('pg');
 const connectionString = 'YourPostgresConnectionStringHere'; // Adjust this accordingly
 
+
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "postgres",
-    password: "postgres",
-    port: 5432, // Default PostgreSQL port
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false, // Note: For secure production setups, provide the CA certificate.
+    },
   });
 
-
-// const pool = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//         rejectUnauthorized: false, // Note: For secure production setups, provide the CA certificate.
-//     },
-//   });
-
-console.log(process.env.DATABASE_URL);
   
 const initTableScript = `
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
